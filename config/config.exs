f06648ca45657f420f68a3cf0242de492d2e575d
cool6662000 <sidviau@gmail.com>
2017-01-28 13:53:06 -0500

    added the github Oauth

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "e6f306269a274cc8f3e9",
+  client_secret: "ec2bbd1a07cde580040a40bdee7b6332bae2c32a"

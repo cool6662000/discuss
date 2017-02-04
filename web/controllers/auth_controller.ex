b82ef0768c3528a5551dd07c486f0514766862ca
cool6662000 <sidviau@gmail.com>
2017-02-03 22:15:50 -0500

    mod auth facebook

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -5,6 +5,11 @@ defmodule Discuss.AuthController do
   alias Discuss.User
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    IO.puts "+++++"
+    IO.inspect(conn.assigns)
+    IO.puts "+++++"
+    IO.inspect(params)
+    IO.puts "+++++"
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
     changeset = User.changeset(%User{}, user_params)
 
